995f14647cba488d1947f04f5b2a6ce5c95671a6
AbhiVardhan020 <abhivardhangoud12345@gmail.com>
2025-06-06 23:41:38 +0530

    cors edit

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "https://nquery-one.vercel.app",
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
